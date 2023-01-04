f22b51cb8d1e4388256de6362303a48924fae5e4
syedsadath-17 <90619459+sadath-12@users.noreply.github.com>
2023-01-04 19:13:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-import './index.css';
-import Layout from './components/Layout';
-import Hero from './components/pages/Home/Hero';
-import HowItWorks from './components/pages/Home/HowItWorks';
-import Section3 from './components/pages/Home/Section3';
-import FAQ from './components/pages/Home/FAQ';
-import Process from './components/pages/Home/Process';
-import Contact from './components/pages/Home/Contact';
-
+import '.../..//index.css';
 function App() {
   return (
     <Layout img='https://www.zonnepanelen.net/wp-content/uploads/zonnepanelen-installateur.jpg'>
