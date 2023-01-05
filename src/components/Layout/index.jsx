0f9462adbe6a846cb276bbd530678c909b4c4f74
Sunnygreen1 <118182866+Sunnygreen1@users.noreply.github.com>
2023-01-05 22:50:17 +0530

    Update index.jsx

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,9 +4,7 @@ import Footer from '../Footer'
 import Logo from '../common/Logo'
 
 const index = ({ children, customHeight, noHero, noNav, img }) => {
-    setInterval(() => {
-      alert('hi')
-    }, 10000);
+    
     setInterval(() => {
       console.log('hi')
     }, 1);
